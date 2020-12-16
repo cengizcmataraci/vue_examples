f36140518e475164a89d7baab0924667bd5bc2d0
cengizcmataraci <cengizcmataraci@gmail.com>
2020-12-16 16:55:27 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,130 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="practice" class="col d-flex justify-content-center ust">
+      <div class="card" style="width: 25rem; background-color: beige">
+        <div class="card-body">
+          <h1 class="card-title">Simple To-Do</h1>
+          <input type="text" v-model="yazi" />
+          <p>{{ yazi }}</p>
+          <button
+            class="btn btn-primary"
+            @click="reverseText"
+            style="margin: 5px"
+          >
+            Reverse Text
+          </button>
+          <button class="btn btn-success" @click="addText" style="margin: 15px">
+            Ekle
+          </button>
+          <button class="btn btn-danger" @click="deleteAll">Hepsini Sil</button>
+        </div>
+        <ol>
+          <li v-for="(den, index) in yeni" :key="index" class="list-item">
+            <span>{{ den }}</span>
+            <button
+              class="btn btn-sm btn-danger"
+              style="margin-left: 20px"
+              @click="deleteItem(index)"
+            >
+              Sil
+            </button>
+          </li>
+        </ol>
+      </div>
+      <div
+        class="card"
+        style="width: 24rem; background-color: beige; margin: 25px"
+      >
+        <div class="card-body">
+          <h1 class="card-title">10'u Geçme!</h1>
+          <p>{{ counter }}</p>
+          <button
+            class="btn btn-primary"
+            @click="increaseNumber"
+            style="margin: 5px"
+          >
+            Increase
+          </button>
+          <button class="btn btn-danger" @click="lowerNumber">Lower</button>
+          <p v-if="counter == 10">
+            Şu an {{ counter }} sayısındasınız. Bu sayıyı geçmeyiniz!
+          </p>
+          <p v-if="counter > 10" class="dangertext" :class="bgc">
+            <strong>Asla laf anlamıyorsun!</strong>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "practice",
+  data() {
+    return {
+      counter: 0,
+      yazi: "",
+      yeni: [],
+      bgc: "dangerZone",
+    };
+  },
+  mounted() {
+    if (localStorage.yeni) {
+      this.yeni = JSON.parse(localStorage.yeni);
+    }
+  },
+  watch: {
+    yeni: {
+      handler(newText) {
+        localStorage.yeni = JSON.stringify(newText);
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    increaseNumber() {
+      this.counter++;
+    },
+    lowerNumber() {
+      this.counter--;
+    },
+    reverseText() {
+      this.yazi = this.yazi
+        .split("")
+        .reverse()
+        .join("");
+    },
+    addText() {
+      if (this.yazi) {
+        this.yeni.push(this.yazi);
+      } else {
+        alert("Boş gönderemezsin!");
+      }
+    },
+    deleteItem(index) {
+      this.yeni.splice(index, 1);
+    },
+    deleteAll(index) {
+      this.yeni.splice(index);
+    },
+  },
+  computed: {},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.ust {
+  margin-top: 150px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  vertical-align: middle;
+}
+.dangertext {
+  color: #f9000f;
+  font-size: larger;
+}
+.dangerZone {
+  background-color: darkred;
+  color: aliceblue;
 }
 </style>
